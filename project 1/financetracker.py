507391552019b78cedbe7ac3a2acade56471898b
venkata manoj <bellamkondavenkatamanoj@gmail.com>
2026-03-10 23:25:03 +0530

    first commit

---
+++ project 1/financetracker.py
@@ -0,0 +1,15 @@
+transactions=[]
+
+
+while True:
+    print("\n 1.Add Transactions ")
+    print(" 2. Add Incomes")
+    print(" 3. View Summary")
+    print(" 4. Exit")
+
+    choice=input("\n Enter choice:").strip()
+
+
+    if choice=="4":
+        print("Good Bye!")
+        break
