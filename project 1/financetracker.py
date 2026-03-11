a0a7fff03408c38c672079841ec8648fb562ad31
venkata manoj <bellamkondavenkatamanoj@gmail.com>
2026-03-11 17:03:53 +0530

    code changed or code edit

--- project 1/financetracker.py
+++ project 1/financetracker.py
@@ -1,43 +1,39 @@
-transactions=[]
-
+transactions = []
 
 while True:
-    print("\n 1.Add Transactions ")
-    print(" 2. Add Incomes")
-    print(" 3. View Summary")
-    print(" 4. Exit")
+    print("\n1. Add Income")
+    print("2. Add Transaction (Expense)")
+    print("3. View Summary")
+    print("4. Exit")
 
-    choice=input("\n Enter choice:").strip()
+    choice = input("\nEnter choice: ").strip()
 
-
-    if choice=="4":
+    if choice == "4":
         print("Good Bye!")
         break
 
-if choice in ("1" & "2"):
-    try:
-        amount = float(input("Amount (rs) :" ))
-        category = input("Category: ").strip()
-        t_type = "income" if choice == "1" else "expense"
+    elif choice in ("1", "2"):
+        try:
+            amount = float(input("Amount (₹): "))
+            category = input("Category: ").strip()
 
-        transactions.append({
-            "type" : t_type,
-            "amount" : amount,
-            "category" : category
-        })
-        print(f"✅ Added {t_type} of ₹{amount}")
-    except ValueError:
-        print("❌ Enter a valid number!")
+            t_type = "income" if choice == "1" else "expense"
 
+            transactions.append({
+                "type": t_type,
+                "amount": amount,
+                "category": category
+            })
 
+            print(f"✅ Added {t_type} of ₹{amount}")
 
-elif choice == "3":
-    income = sum(t["amount"] for t in transactions if t["type"] == "income")
-    expenses = sum(t["amount"] for t in transactions if t["type"] == "expense")
+        except ValueError:
+            print("❌ Enter a valid number!")
 
-    print(f"\n💚 Income:   ₹{income:.2f}")
-    print(f"🔴 Expenses: ₹{expenses:.2f}")
-    print(f"💰 Balance:  ₹{income - expenses:.2f}")
+    elif choice == "3":
+        income = sum(t["amount"] for t in transactions if t["type"] == "income")
+        expenses = sum(t["amount"] for t in transactions if t["type"] == "expense")
 
-
-    
+        print(f"\n💚 Income:   ₹{income:.2f}")
+        print(f"🔴 Expenses: ₹{expenses:.2f}")
+        print(f"💰 Balance:  ₹{income - expenses:.2f}")
