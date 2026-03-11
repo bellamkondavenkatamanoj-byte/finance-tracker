8629ded949b1be22bb70c402304f1f6ace5b40ed
venkata manoj <bellamkondavenkatamanoj@gmail.com>
2026-03-11 16:53:16 +0530

    Added transaction feature

--- project 1/financetracker.py
+++ project 1/financetracker.py
@@ -13,3 +13,31 @@ while True:
     if choice=="4":
         print("Good Bye!")
         break
+
+if choice in ("1" & "2"):
+    try:
+        amount = float(input("Amount (rs) :" ))
+        category = input("Category: ").strip()
+        t_type = "income" if choice == "1" else "expense"
+
+        transactions.append({
+            "type" : t_type,
+            "amount" : amount,
+            "category" : category
+        })
+        print(f"✅ Added {t_type} of ₹{amount}")
+    except ValueError:
+        print("❌ Enter a valid number!")
+
+
+
+elif choice == "3":
+    income = sum(t["amount"] for t in transactions if t["type"] == "income")
+    expenses = sum(t["amount"] for t in transactions if t["type"] == "expense")
+
+    print(f"\n💚 Income:   ₹{income:.2f}")
+    print(f"🔴 Expenses: ₹{expenses:.2f}")
+    print(f"💰 Balance:  ₹{income - expenses:.2f}")
+
+
+    
